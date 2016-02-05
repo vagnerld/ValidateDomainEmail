6d5125158ae9fe1569d4ff8eb092a13f65ead689
Vagner Dutra <vagnerdutra@outlook.com>
2016-02-05 16:02:52 -0200

    atualizando classe e criando tabela de comparações por porcentagem

--- EmailChecker.php
+++ EmailChecker.php
@@ -2,18 +2,19 @@
 
 class ValidateDomainEmail {
 
-    private $domains_default = array(
-        "yahoo", "hotmail", "gmail", "live", "outlook", "msn"
+    private $domains_list = array(
+        "yahoo", "hotmail", "gmail", "live", "outlook", "msn", "globo", "uol"
     );
 
     private function explodeEmail($email){
-       $d = explode('@', $email);
+        $d = explode('@', $email);
         $name = $d[0];
         $e = explode('.', $d[1]);
         $domain = $e[0];
         $final = $e[1];
         if($e[2]){ $final .= ".".$e[2]; }
-    
+        if($e[3]){ $final .= ".".$e[3]; }
+
         return array(
                     'name'=>$name,
                     'domain'=>$domain,
@@ -23,22 +24,43 @@ class ValidateDomainEmail {
 
     public function check($email) {
         $d = $this->explodeEmail($email);
+        $name_email = $d['name'];
+        $domain_email = $d['domain'];
+        $final_email = $d['final'];
+
+        foreach ($this->domains_list as $domain) {
+            similar_text($domain_email, $domain, $percent);
+            if($percent > 75) {
+                $email = $name_email."@".$domain.".".$final_email;
+                break;
+            }
+        }
+        return $email;
+    }
+
+    public function print_table($email) {
+
+        echo "<table style='border-collapse: collapse; margin: 0px auto;'>";
+
+        $d = $this->explodeEmail($email);
 
         $name_email = $d['name'];
         $domain_email = $d['domain'];
         $final_email = $d['final'];
 
-        foreach ($this->domains_default as $key => $value) {
-            similar_text($domain_email, $value, $percent);
-            echo $value.": ".$percent."%<br />";
+        foreach ($this->domains_list as $domain) {
+            similar_text($domain_email, $domain, $percent);
             if($percent > 75) {
-                $email = $name_email."@".$value.".".$final_email;
-                //break;
+                echo "<tr style='background:#8BC34A;'><td style='border: 1px solid #ccc; padding: 10px;'>".$domain."</td><td style='border: 1px solid #ccc; padding: 10px;'>".$percent."% </td></tr>";
+            } else {
+                echo "<tr><td style='border: 1px solid #ccc; padding: 10px;'>".$domain."</td><td style='border: 1px solid #ccc; padding: 10px;'>".$percent."% </td></tr>";
             }
         }
 
-        return $email;
+        echo "</table>";
+
     }
+
 }
 
 ?>
--- index.php
+++ index.php
@@ -1,23 +1,18 @@
-<?php
-
-
-require_once "./EmailChecker.php"; // lib 
-$e = new ValidateDomainEmail();
-
-
-$email = $_REQUEST['email'];
-
-echo $email."<br /><br />";
-
-echo "<br />Voce quis dizer: <b>".$e->check($email)."?";
-
-
-
-
+<?php 
+	$email = $_REQUEST['email'];
 ?>
+<body style="text-align:center;">
+	<br /><br /><br /><br /><br />
+	<form method="post">
+		<input type="text" name="email" value="<?php echo $email; ?>">
+		<input type="submit">
+	</form>
 
-<br /><br /><br /><br /><br />
-<form>
-<input type="text" name="email" value="<?php echo $email; ?>">
-<input type="submit">
-</form>
+	<?php 
+		require_once "./EmailChecker.php"; // lib 
+		$e = new ValidateDomainEmail();
+		echo "<br />Voce quis dizer: <b>".$e->check($email)."</b>? <br /><br /><br />";
+
+		$e->print_table($email);
+	?>
+</body>
