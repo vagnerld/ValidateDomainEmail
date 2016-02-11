08b40394bac0fd764c85c0c52a55ddafebe84a1b
Vagner Dutra <vagnerdutra@outlook.com>
2016-02-11 13:32:13 -0200

    otimização da função check

--- EmailChecker.php
+++ EmailChecker.php
@@ -17,12 +17,9 @@
 
         private function explodeEmail($email){
             $d = explode('@', $email);
-            $name = $d[0];
-            $domain = $d[1];
-
             return array(
-                        'name'=>$name,
-                        'domain'=>$domain
+                        'name'=>$d[0],
+                        'domain'=>$d[1]
                     );
         }
 
@@ -31,18 +28,16 @@
             $name_email = $d['name'];
             $domain_email = $d['domain'];
 
-            $complete = false;
-            $input_domains = array();
+            $selected_domains = array();
             foreach ($this->list_domains as $domain) {
                 similar_text($domain_email, $domain, $percent);
-                if($percent > $this->precision) {
-                    $input_domains[$domain] = $percent;
-                    $complete = true;
+                if($percent >= $this->precision) {
+                    $selected_domains[$domain] = $percent;
                 }
             }
-            if ($complete) {
-                arsort($input_domains);
-                $email = $name_email."@".key($input_domains);
+            if ($selected_domains) {
+                arsort($selected_domains);
+                $email = $name_email."@".key($selected_domains);
             } else {
                 $email = $name_email."@".$domain_email;
             }
@@ -54,10 +49,7 @@
             $str .= "<table style='border-collapse: collapse; margin: 0px auto;'>";
             $str .= "<tr><td style='border: 1px solid #ccc; padding: 10px;' COLSPAN=2> <b>Precisão de ".$this->precision."% </b></td></tr>";
             $d = $this->explodeEmail($email);
-
-            $name_email = $d['name'];
             $domain_email = $d['domain'];
-            $final_email = $d['final'];
 
             foreach ($this->list_domains as $domain) {
                 similar_text($domain_email, $domain, $percent);
@@ -67,7 +59,6 @@
                     $str .= "<tr><td style='border: 1px solid #ccc; padding: 10px;'>".$domain."</td><td style='border: 1px solid #ccc; padding: 10px;'>".$percent."% </td></tr>";
                 }
             }
-
             $str .= "</table>";
 
             return $str;
